7de5e869e58aa875543b266b21be72c4ca21e00a
Keenan Gebze <keenan.gebze@gmail.com>
2018-07-03 22:30:38 +0700

    Add tensorflow dependencies and fixing the dataset download path. Ignore .csv files in gitignore.

--- eager/eager.py
+++ eager/eager.py
@@ -1,7 +1,9 @@
-import os
+import os, sys
+import tensorflow as tf
 train_dataset_url = "http://download.tensorflow.org/data/iris_training.csv"
 filename = os.path.basename(train_dataset_url)
-if os.path.exists(filename):
-  train_dataset_fp = tf.keras.utils.get_file(fname=filename, origin=train_dataset_url)
+if not os.path.exists(filename):
+  train_dataset_fp = tf.keras.utils.get_file(fname=os.path.join(sys.path[0], filename), origin=train_dataset_url)
 print("Local copy of the dataset file: {}".format(filename))
+
 
